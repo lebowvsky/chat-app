648ed2260d6f0c8e01d836606081f413e37a0e0f
Brice Le Gallo <lebowvsky@gmail.com>
2020-03-31 09:17:17 +0200

    Code plus joli

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import ContactList from './components/ContactList';
-import Contact from './components/Contact';
 import { elementType } from 'prop-types';
 
+const users = [
+  {
+    name: 'Robert Reyes',
+    avatar: 'https://randomuser.me/api/portraits/men/28.jpg',
+    online: false
+  },
+  {
+    name: 'Nellie Caldwell',
+    avatar: 'https://randomuser.me/api/portraits/women/17.jpg',
+    online: true
+  },
+  {
+    name: 'Vernon Mason',
+    avatar: 'https://randomuser.me/api/portraits/men/84.jpg',
+    online: true
+  },
+  {
+    name: 'Erica Hunt',
+    avatar: 'https://randomuser.me/api/portraits/women/87.jpg',
+    online: false
+  },
+  {
+    name: 'Juanita Phillips',
+    avatar: 'https://randomuser.me/api/portraits/women/55.jpg',
+    online: true
+  }
+];
 
-
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <ContactList />
+      {users.map(elt => (
+        <ContactList {...elt} />
+      ))}  
     </div>
   );
 }
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,50 +1,25 @@
 import React from 'react';
 import './Contact.css';
 
-const users = [
-    {
-      name: 'Robert Reyes',
-      avatar: 'https://randomuser.me/api/portraits/men/28.jpg',
-      online: false
-    },
-    {
-      name: 'Nellie Caldwell',
-      avatar: 'https://randomuser.me/api/portraits/women/17.jpg',
-      online: true
-    },
-    {
-      name: 'Vernon Mason',
-      avatar: 'https://randomuser.me/api/portraits/men/84.jpg',
-      online: true
-    },
-    {
-      name: 'Erica Hunt',
-      avatar: 'https://randomuser.me/api/portraits/women/87.jpg',
-      online: false
-    },
-    {
-      name: 'Juanita Phillips',
-      avatar: 'https://randomuser.me/api/portraits/women/55.jpg',
-      online: true
-    }
-  ];
+
   
 
-const ContactList = () => (
-    
-    users.map(elt => (
+const ContactList = ({name, avatar, online}) => {
+
+     return (
+        
         <div className="Contact">
-            <img className="avatar" src={elt.avatar} alt="" />
+            <img className="avatar" src={avatar} alt="" />
             <div>
-                <h4 className="name">{elt.name}</h4>
+                <h4 className="name">{name}</h4>
                 <div className="status">
-                    <div className={elt.online? "status-online" : "status-offline"}></div>
-                    <p className="status-text">{elt.online ? "Online" : "Offline"}</p>
+                    <div className={online? "status-online" : "status-offline"}></div>
+                    <p className="status-text">{online ? "Online" : "Offline"}</p>
                 </div>
             </div>
         </div>
-    ))      
-)
-
+        
+      )
+}
 
 export default ContactList;
